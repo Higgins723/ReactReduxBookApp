7e127d242d12ec03d8b318c9d7294d73b064ebb1
Joshua-Higgins <joshua.higgins@rakuten.com>
2018-03-20 11:45:10 -0600

    first push

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'book 1', pages: 100 },
+        { title: 'book 2', pages: 200 },
+        { title: 'book 3', pages: 300 },
+        { title: 'book 4', pages: 400 }
+    ]
+}
